0d9925cd10768db7f53bf186f23670bee72a32c7
Lee <97833081+qcqced123@users.noreply.github.com>
2023-05-06 20:19:43 +0900

    baekjoon 12904

--- baekjoon/greedy/baekjoon_12904.py
+++ baekjoon/greedy/baekjoon_12904.py
@@ -1,7 +1,7 @@
 import sys
 """
 [풀이 시간]
-1) 19:15 ~ 19:45
+1) 19:45 ~ 20:15
 
 [요약]
 1) 두 문자열 S와 T가 주어졌을 때, S를 T로 바꾸는 게임
@@ -12,8 +12,26 @@ import sys
 1) 서로 이웃한 알파벳 사이의 관계가 중요함
     - 각 단계 마다 문자열 T동일한 관계성을 갖게 되는지 판정 여부 따져 보기
     - 자료 구조는 리스트 활용!
+=> 모르겠다 답지를 보자
+=> 알고리즘에서 리스트를 뒤집겠다는 것은 값을 죄다 반전 시키는게 아니라 진짜 그냥 순서를 뒤집겠다는 의미
+[Solution]
+1) S를 T에 맞게 만드는 것은 경우의 수가 너무 많아지기 때문에, T를 S로 만들자!
 """
 S = list(sys.stdin.readline().rstrip())
 T = list(sys.stdin.readline().rstrip())
+result = 0
 
-print(S, T)
+for i in range(len(T)-1, -1, -1):
+    if S == T:
+        result = 1
+        break
+
+    if T[i] == 'A':
+        T.pop()
+
+    else:
+        T.pop()
+        T.reverse()  # list를 거꾸로
+print(result)
+
+
