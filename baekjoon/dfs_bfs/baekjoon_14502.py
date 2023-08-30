14bea4a1fee78635adf1001077752edca7c461b1
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-08-30 19:46:43 +0900

    Add bakejoon 14502

--- baekjoon/dfs_bfs/baekjoon_14502.py
+++ baekjoon/dfs_bfs/baekjoon_14502.py
@@ -1,63 +1,69 @@
-from collections import deque
 import sys
-"""
-
-[Situation]
-1) 바이러스 확산을 막기 위해 가벽을 세울 예정
-2) 연구소 크기 => N * M
-3) 간선 가중치 => 1
-    => BFS 사용하자!
-4) 바이러스는 상하좌우로 이동 가능, 인접한 빈 칸으로 모두 이동 가능함
-5) 새로 세울 수 있는 가벽의 수는 3개, 3개 다 세워야 함 적거나 많게 세울 수 없음
-
-[Representation]
-0 => 빈칸
-1 => 벽
-2 => 바이러스가 들어있는 공간
-안전 영역 => 바이러스가 더 이상 퍼질 수 없는 공간
-
-[Goal]
-연구소 지도가 주어졌을 때, 가벽 3개를 세워서 얻을 수 있는 안전 영역 크기의 최대값
-
-[Strategy]
-즉, 현재 0으로 표시된 구역에 가벽을 세울 수 있으며, 0의 개수가 가장 많도록 가벽을 세우면 되겠네 
-간선 가중치가 1이라는 것에서 BFS를 활용해야 함을 알 수 있음
-이미 값으로 테이블을 초기화 시키기 때문에, 기타 다른 자료형을 선언할 필요가 없고 그대로 사용하면 된다.
+from collections import deque
+from typing import List
+from itertools import combinations
+from copy import deepcopy
 
 """
-
-# Step 1. Input Init
-N, M = map(int, sys.stdin.readline().split())
-graph = [list(map(int, sys.stdin.readline().split())) for row in range(N)]
-
-print(graph)
-
-dx = [-1, 1, 0, 0] # 좌우
-dy = [0, 0, -1, 1] # 상하
-
-# Step 2. BFS
-queue = deque()
-queue.append((4,1))
-
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        # 직사각형 밖으로 나가는 경우
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
-            continue
-
-        # 진행 방향에 가벽이 존재하는 경우
-        if graph[nx][ny] == 1:
-            continue
-
-        # 진행 방항에 가벽이 없는 경우, 진행 방향에 이미 바이러스가 존재하는 경우
-        if graph[nx][ny] == 0:
-            graph[nx][ny] = 2
-
-        queue.append((nx, ny))
+[시간]
+1) 18:10 ~ 18:55
+[요약]
+1) 바이러스의 확산을 막기 위해서 연구소에 가벽 세워 안전 영역이 최대가 되도록 하는 프로그램 작성
+    - 바이러스: 상하좌우, 빈 칸으로 이동 가능
+    - 가벽은 꼭 3개 다 써야함
+    - 0, 1, 2: 빈 칸, 벽, 바이러스
+    - 안전 영역: 가벽을 모두 세운 뒤, 바이러스가 더 이상 퍼질 수 없는 구역
+[전략]
+1) BFS + BF
+    - 입력이 짧고 시간 제한이 길어 여유로움
+2) BFS + Greedy
+"""
 
 
-print(graph)
+def bfs(y: int, x: int, virus_map: List[List[int]], visit: List[bool]):
+    visit[y][x] = True
+    queue = deque()
+    queue.append([y, x])
+    while queue:
+        vy, vx = queue.popleft()
+        for idx in range(4):
+            ny = dy[idx] + vy
+            nx = dx[idx] + vx
+            if -1 < ny < N and -1 < nx < M and virus_map[ny][nx] == 0 and not visit[ny][nx]:
+                visit[ny][nx] = True
+                queue.append([ny, nx])
+                virus_map[ny][nx] = 2
+
+
+N, M = map(int, sys.stdin.readline().split())  # row, col
+lab = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+
+# Step 1. find wall index
+result = []
+wall_list = []
+for i in range(N):
+    for j in range(M):
+        if lab[i][j] == 0:
+            wall_list.append([i, j])
+
+# Step 2. make additional wall & move virus
+for simulation in list(combinations(wall_list, 3)):
+    tmp_lab = deepcopy(lab)
+    visited = [[False] * M for _ in range(N)]
+    wall1, wall2, wall3 = simulation[0], simulation[1], simulation[2]
+    tmp_lab[wall1[0]][wall1[1]], tmp_lab[wall2[0]][wall2[1]], tmp_lab[wall3[0]][wall3[1]] = 1, 1, 1
+    for i in range(N):
+        for j in range(M):
+            if not visited[i][j] and tmp_lab[i][j] == 2:
+                bfs(i, j, tmp_lab, visited)
+    # Step 3. calculate score
+    tmp_result = 0
+    for i in range(N):
+        for j in range(M):
+            if tmp_lab[i][j] == 0:
+                tmp_result += 1
+    result.append(tmp_result)
+print(max(result))
