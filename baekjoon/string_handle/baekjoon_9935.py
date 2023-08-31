c2c14050dc2b73012554fcb364eb5e7dc08535d6
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-08-31 17:29:05 +0900

    Update baekjoon_9935.py

--- baekjoon/string_handle/baekjoon_9935.py
+++ baekjoon/string_handle/baekjoon_9935.py
@@ -1,7 +1,5 @@
 import sys
 """
-[시간]
-1) 01:05 ~ 01:20
 [요약]
 1) 폭발 문자열: 폭발하면 그 문자는 문자열에서 사라지며, 남은 문자열은 합쳐지게 된다
     - 문자열이 폭발 문자열을 포함하고 있는 경우에, 모든 폭발 문자열이 폭발하게 된다. 남은 문자열을 순서대로 이어 붙여 새로운 문자열
@@ -14,13 +12,14 @@ import sys
     - 입력 길이: 100만 (설계 알고리즘 < O(n^2))
     - replace: O(n)
     => 이거보다 더 빠른게 필요
+2) stack + linear search + del
+    - 문자열을 순차적으로 스택에 할당
 """
 text, bomb = sys.stdin.readline().rstrip(), sys.stdin.readline().rstrip()
-iter_checker = True
-complete, sub_text = text[:text.index(bomb[0])], text[text.index(bomb[0]):]
-while iter_checker:
-    if bomb not in sub_text:
-        break
-    sub_text = sub_text.replace(bomb, '')
-text = complete + sub_text
-print(text if len(text) else 'FRULA')
+stack = []
+
+for char in text:
+    stack.append(char)
+    if ''.join(stack[-len(bomb):]) == bomb:
+        del stack[-len(bomb):]
+print(''.join(stack) if len(stack) else "FRULA")
