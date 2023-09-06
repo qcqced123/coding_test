812c044ff59447aa643d0ebe578ab2cff08a44dc
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-09-06 14:42:25 +0900

    Update baekjoon_11727.py

--- baekjoon/dynamic_programming/baekjoon_11727.py
+++ baekjoon/dynamic_programming/baekjoon_11727.py
@@ -1,4 +1,6 @@
 import sys
+from typing import List
+
 """
 [요약]
 1) 2×n 직사각형을 1×2, 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성
@@ -7,4 +9,17 @@ import sys
 [전략]
 1) 시간이 짭고 메모리 제한이 크기 때문에 Tabulation
 """
+
+
+def get_tiling(x: int, arr: List[int]) -> int:
+    if x > 2:
+        for i in range(3, x+1):
+            arr[i] = arr[i-1] + 2*arr[i-2]
+    return arr[x]
+
+
 N = int(sys.stdin.readline())
+tiling = [0] * (N+1)
+tiling[1], tiling[2] = 1, 3
+print(get_tiling(N, tiling) % 10007)
+
