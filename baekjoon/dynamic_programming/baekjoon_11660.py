7a8142fc74a12b9d50b2724c31c3aced9c6dc1b0
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-09-20 00:51:49 +0900

    Add Week 3-1 Solve

--- baekjoon/dynamic_programming/baekjoon_11660.py
+++ baekjoon/dynamic_programming/baekjoon_11660.py
@@ -7,8 +7,20 @@ import sys
     - 시간, 메모리, 입력 길이: 1초, 256, 1024
     => Tabulation 가능
 [전략]
-1) dp[end] = end까지 누적합 - (src-1)까지 누적합
+1) dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j]
     - 누적합 배열 만들기
-    -
+    - 2차원 누적 합은 1차원 누적 합과 완전히 다른 개념!
+    - 1행, 1열은 1차원 배열의 누적값으로 먼저 초기화
 """
 N, M = map(int, sys.stdin.readline().split())
+graph, dp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)], [[0] * (N+1) for _ in range(N+1)]
+
+# 1) init row 0 & col 0
+for i in range(N):
+    for j in range(N):
+        dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1] - dp[i][j] + graph[i][j]  # Cautions! index are different between graph and dp
+
+# 2) calculate target value
+for _ in range(M):
+    row1, col1, row2, col2 = map(int, sys.stdin.readline().split())
+    print(dp[row2][col2] - dp[row1-1][col2] - dp[row2][col1-1] + dp[row1-1][col1-1])
