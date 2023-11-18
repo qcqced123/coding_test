c83037cd86879ffc06fe370113fb89f6a00c6faa
JungHwan Lee <qcqced123@gmail.com>
2023-11-18 09:19:18 +0900

    Create baekjoon_1351.py

---
+++ baekjoon/dynamic_programming/baekjoon_1351.py
@@ -0,0 +1,20 @@
+import sys
+from typing import List
+"""
+[조건]
+- A0 = 1
+- Ai = A⌊i/P⌋ + A⌊i/Q⌋ (i ≥ 1)
+[요약]
+DP
+1) 
+
+"""
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    N, P, Q = map(int, sys.stdin.readline().split())
+    solution()
