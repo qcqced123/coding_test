8d06da7dfc9d6d4022cbe089fd0d05ee989e01d8
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-02-01 17:41:41 +0900

    Update baekjoon_2579.py

--- baekjoon/dynamic_programming/baekjoon_2579.py
+++ baekjoon/dynamic_programming/baekjoon_2579.py
@@ -1,10 +1,18 @@
 import sys
-from typing import List
-"""
-[풀이]
-1)
-"""
 
 
 n = int(sys.stdin.readline())
-stair = [int(sys.stdin.readline()) for _ in range(n)]  # 시작점 추가 여부
+stair = [0] + [int(sys.stdin.readline()) for _ in range(n)]
+
+if n == 1 or n == 2:
+    print(sum(stair))
+    exit()
+
+dp = [0] * (n+1)
+
+dp[1] = stair[1]
+dp[2] = stair[1] + stair[2]
+
+for i in range(3, n+1):
+    dp[i] = max(dp[i-2]+stair[i], dp[i-3]+stair[i-1]+stair[i])
+print(dp[n])
