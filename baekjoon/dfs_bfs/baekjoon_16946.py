94bddeb1a899dab0ce769fa15f47edb655d207c4
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-02-21 11:24:08 +0900

    add

--- baekjoon/dfs_bfs/baekjoon_16946.py
+++ baekjoon/dfs_bfs/baekjoon_16946.py
@@ -1,38 +1,67 @@
 import sys
 from collections import deque
-from typing import List
 
 
-def bfs(y: int, x: int, graph: List[List]) -> int:
-    visit, count = set(), 1
-    visit.add((y, x))
-    q = deque([(y, x)])
+def bfs(y: int, x: int, z: int, graph, visited, area_dict):
     dy, dx = (-1, 1, 0, 0), (0, 0, -1, 1)
+    visited[y][x], graph[y][x], total = True, z, 1
+    q = deque([(y, x)])
     while q:
         vy, vx = q.popleft()
         for i in range(4):
             ny, nx = dy[i] + vy, dx[i] + vx
-            if -1 < ny < len(graph) and -1 < nx < len(graph[0]) and (ny, nx) not in visit and not graph[ny][nx]:
-                count += 1
-                visit.add((ny, nx))
+            if -1 < ny < len(graph) and -1 < nx < len(graph[0]) and not visited[ny][nx] and not graph[ny][nx]:
+                total += 1
                 q.append((ny, nx))
-    return count
+                visited[ny][nx] = True
+                graph[ny][nx] = z
+    area_dict[z] = total
+
+
+def calculate(y: int, x: int, graph, area_dict):
+    group_set = set()
+    graph[y][x] = 1
+    dy, dx = (-1, 1, 0, 0), (0, 0, -1, 1)
+    for i in range(4):
+        ny, nx = dy[i] + y, dx[i] + x
+        if -1 < ny < len(graph) and -1 < nx < len(graph[0]) and graph[ny][nx] < -1:
+            area = graph[ny][nx]
+            group_set.add(area)
+
+    for k in group_set:
+        graph[y][x] += area_dict[k]
+    graph[y][x] %= 10
 
 
 def solution():
     N, M = map(int, sys.stdin.readline().split())
-    grid, walls = [], set()
+    grid, areas, walls, visit = [], set(), set(), [[False] * M for _ in range(N)]
     for i in range(N):
-        col = list(map(int,
-                       sys.stdin.readline().rstrip()))
+        col = list(map(int, sys.stdin.readline().rstrip()))
         grid.append(col)
         for j in range(M):
-            if col[j] == 1:
+            if not col[j]:
+                areas.add((i, j))
+
+            elif col[j] == 1:
+                grid[i][j] = -1
                 walls.add((i, j))
+    area = {}
+    area_num = -2
+    for coordinate in areas:
+        r, c = coordinate
+        if not grid[r][c]:
+            bfs(r, c, area_num, grid, visit, area)
+            area_num -= 1
 
     for coordinate in walls:
         y, x = coordinate
-        grid[y][x] = bfs(y, x, grid) % 10
+        calculate(y, x, grid, area)
+
+    for r in range(N):  # 여기도 최적화 가능할 것 같고
+        for c in range(M):
+            if grid[r][c] < 0:
+                grid[r][c] = 0
 
     for i in range(N):
         print(''.join(map(str, grid[i])), end='\n')
--- baekjoon/implementation/baekjoon_17070.py
+++ baekjoon/implementation/baekjoon_17070.py
@@ -1,36 +1,35 @@
 import sys
+from typing import List
 
 
-def dfs(d: int, y: int, x: int) -> None:
-    global count, dy, dx, grid, N
-    if y == N - 1 and x == N - 1:
-        count += 1
-        return
+def solution():
+    N = int(sys.stdin.readline())
+    grid, dp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)], [[0]*N for _ in range(N)]
 
-    if not d:
-        ty, tx = dy[0:2], dx[0:2]
-    elif d == 1:
-        ty, tx = dy, dx
-    else:
-        ty, tx = dy[1:], dx[1:]
+    for c in range(N):
+        if not grid[0][c]:
+            dp[0][c] = 1
+    for c in range(2, N):
+        if not grid[1][c] and not grid[1][c-1] and not grid[0][c]:
+            dp[1][c] = 1
 
-    for i in range(len(ty)):
-        nd = i if not d or d == 1 else i+1
-        ny, nx = ty[i] + y, tx[i] + x
+    for i in range(len(grid)):
+        print(grid[i], end='\n')
 
-        if -1 < ny < N and 0 < nx < N and nd == 1 and not grid[ny][nx] and not grid[ny-1][nx] and not grid[ny][nx-1]:
-            dfs(nd, ny, nx)
-
-        elif -1 < ny < N and 0 < nx < N and nd != 1 and not grid[ny][nx]:
-            dfs(nd, ny, nx)
+    for i in range(len(dp)):
+        print(dp[i], end='\n')
 
 
-N = int(sys.stdin.readline())
-grid = tuple(tuple(map(int, sys.stdin.readline().split())) for _ in range(N))
-dy, dx, count = (1, 1, 0), (0, 1, 1), 0
+    for i in range(2, N):
+        for j in range(2, N):
+            if grid[i][j] == 1:
+                continue
+            dp[i][j] = dp[i][j-1]  # 가로
+            if
 
-if grid[N-1][N-1]:
-    print(0)
-    exit()
-dfs(2, 0, 1)
-print(count)
+
+
+
+
+if __name__ == "__main__":
+    solution()
