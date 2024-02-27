7e1e943eafb58851a285e2268c388731cd952034
JungHwan Lee <qcqced123@gmail.com>
2024-02-27 20:51:38 +0900

    Update baekjoon_5639.py

--- baekjoon/data_structure/baekjoon_5639.py
+++ baekjoon/data_structure/baekjoon_5639.py
@@ -1,12 +1,30 @@
 import sys
-from collections import defaultdict
 
 
-sys.setrecursionlimit(10**6)
-nodes, tree = [], defaultdict(list)
-while True:
-    try: nodes.append(int(sys.stdin.readline()))
-    except: break
+def solution():
+    sys.setrecursionlimit(10**6)
+    nodes = []
+    while True:
+        try: nodes.append(int(sys.stdin.readline()))
+        except: break
 
-nodes.sort()
+    def postorder(src, end):
+        if src > end:
+            return
+
+        root = nodes[src]
+        new_end = end + 1
+        for i in range(src+1, end+1):
+            if nodes[i] > root:
+                new_end = i
+                break
+        postorder(src+1, new_end-1)
+        postorder(new_end, end)
+        print(root)
+
+    postorder(0, len(nodes)-1)
+
+
+if __name__ == '__main__':
+    solution()
 
