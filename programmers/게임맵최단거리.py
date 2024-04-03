c32f6c9437eef27d50828fff5dd6b46c5fee2303
JungHwan Lee <qcqced123@gmail.com>
2024-04-03 22:28:25 +0900

    add

--- programmers/게임맵최단거리.py
+++ programmers/게임맵최단거리.py
@@ -1,15 +1,43 @@
-from typing import Tuple, List
+from collections import deque
 
 
-def solution():
+def solution(maps):
     """
     problem link:
+        https://school.programmers.co.kr/learn/courses/30/lessons/1844
 
     summary:
-
-    args:
+        1) 상대 팀 진영 먼저 파괴(도착)
+          - 누가 더 빨리 도착하는가 == BFS
 
     solution:
+        1) 2D Table with BFS
     """
-    result = []
-    return result
+
+    def bfs(y: int, x: int, graph) -> int:
+
+        def is_goal(curr_y, curr_x):
+            return curr_y == len(graph) - 1 and curr_x == len(graph[0]) - 1
+
+        def is_valid(curr_y, curr_x):
+            return -1 < curr_y < len(graph) and -1 < curr_x < len(graph[0]) and not visit[curr_y][curr_x] and \
+                graph[curr_y][curr_x]
+
+        visit, queue = [[0] * len(graph[0]) for _ in range(len(graph))], deque([(1, y, x)])  # 시간, 좌표
+        visit[y][x] = 1
+        while queue:
+            vt, vy, vx = queue.popleft()
+            for i in range(4):
+                nt, ny, nx = vt + 1, vy + dy[i], vx + dx[i]
+                if is_valid(ny, nx):
+                    if is_goal(ny, nx):
+                        return nt
+
+                    queue.append((nt, ny, nx));
+                    visit[ny][nx] = 1
+        return -1
+
+    dy, dx = (-1, 1, 0, 0), (0, 0, -1, 1)
+    answer = bfs(0, 0, maps)
+    return answer
+
--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -1,15 +1,43 @@
-from typing import Tuple, List
+import sys
 
 
-def solution():
+def my_solution1(n, computers):
     """
     problem link:
+        https://school.programmers.co.kr/learn/courses/30/lessons/43162
 
     summary:
+        1) 같은 네트워크 상에 있다
+          - 같은 그래프 안에 있다
 
-    args:
+    return:
+        1) 상호 베타적인 그래프 혹은 집합이 몇 개 있음??
 
     solution:
+        1) DFS 탐색
+          - 인접 리스트 만들기
+            - 키값과 같은 인덱스 넘어가기
+            - 배열값 0 넘어가기
+
     """
-    result = []
-    return result
+    answer = 0
+    visit = []
+    graph = {i: j for i, j in enumerate(computers)}
+
+    sys.setrecursionlimit(10 ** 6)
+
+    def is_valid(curr_node, curr_state, node):
+        return node != curr_node and curr_state and curr_node not in visit
+
+    def dfs(x: int):
+        for i, j in enumerate(graph[x]):
+            if is_valid(i, j, x):
+                visit.append(i)
+                dfs(j)
+
+    for k in graph.keys():
+        if k not in visit:
+            visit.append(k); answer += 1
+            dfs(k)
+    return answer
+
