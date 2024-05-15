1b8dbdcbd3613d878ad25a412d3c41ed42b6ec5e
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-05-15 10:16:45 +0900

    Update 피로도.py

--- programmers/피로도.py
+++ programmers/피로도.py
@@ -1,19 +1,35 @@
-from typing import Tuple, List
+import sys
+from typing import List
 
 
-def solution():
+def solution(k, dungeons):
     """
     problem link:
-
-    args:
+        https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3
 
     solution:
+        1) 최소 필요 피로도
+        2) 소모 피로도
+        => 최대한 많은 던전 탐험하기
 
     implementation:
+        1) Backtracking
+
     """
-    result = []
-    return result
+    answer = [0]
+    sys.setrecursionlimit(10 ** 6)
+
+    def backtracking(src: int, candidates: List, result: int):
+        for i in range(len(candidates)):
+            lower_bound, consume = candidates[i]
+            if src >= lower_bound:
+                backtracking(src - consume, candidates[:i] + candidates[i + 1:], result + 1)
+
+        answer[0] = max(answer[0], result)
+
+    backtracking(k, dungeons, 0)
+    return answer[0]
 
 
 if __name__ == '__main__':
-    solution()
+    solution(100, [[100, 100], [100, 100], [100, 100], [100, 100], [100, 100]])
