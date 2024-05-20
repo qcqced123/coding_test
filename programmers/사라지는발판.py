78712095aed8208184f236f3c3aa2b87376007d8
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-05-20 16:43:37 +0900

    week 1-1 solve

--- programmers/사라지는발판.py
+++ programmers/사라지는발판.py
@@ -1,25 +1,33 @@
-import sys
 import copy
 
 
 def solution(board, aloc, bloc):
-    answer = 0
-
-    def build_visit_arr(r: int, c: int):
-        return [[[0, 0] for _ in range(c)] for _ in range(r)]
-
-    visit = build_visit_arr(len(board), len(board[0]))
-
     dy, dx = (-1, 1, 0, 0), (0, 0, -1, 1)
 
-    def is_valid(curr_y, curr_x):
-        return
+    def is_valid(y, x, curr_y, curr_x, curr_grid):
+        return curr_grid[y][x] and -1 < curr_y < len(curr_grid) and -1 < curr_x < len(curr_grid[0]) and \
+            curr_grid[curr_y][curr_x]
 
-    def game_turn(grid, ay, ax, by, bx, turn):
+    def game_turn(grid, ay, ax, by, bx):
+        count, result = 0, 0
         curr_board = copy.deepcopy(grid)
 
         for i in range(4):
             nay, nax = dy[i] + ay, dx[i] + ax
-            if
+            if is_valid(ay, ax, nay, nax, curr_board):
+                count += 1
+                for j in range(4):
+                    nby, nbx = dy[i] + by, dx[i] + bx
+                    if is_valid(by, bx, nby, nbx, curr_board):
+                        count += 1
+                        curr_board[ay][ax], curr_board[by][bx] = 0, 0
+                        count += game_turn(curr_board, nay, nax, nby, nbx)
+                        curr_board[ay][ax], curr_board[by][bx] = 1, 1  # backtracking
+        return count
 
+    answer = game_turn(board, aloc[0], aloc[1], bloc[0], bloc[1])
     return answer
+
+
+if __name__ == '__main__':
+    solution([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [1, 0], [1, 2])
--- programmers/외벽점검.py
+++ programmers/외벽점검.py
@@ -1,20 +1,32 @@
-from typing import Tuple, List
-from itertools import combinations
+from itertools import permutations
 
 
-def solution():
+def solution(n, weak, dist):
     """
-    problem link:
-
-    args:
-
-    solution:
-
-    implementation:
+    Implementation:
+        1) 1시간 이동 거리처리
+        2) 이동 방향
+        3) 친구들 배치 순서
     """
-    result = []
-    return result
+    length = len(weak)
+    answer = len(dist) + 1
 
+    def extend_arr(arr):
+        return arr[:] + [i + n for i in arr]
 
-if __name__ == '__main__':
-    solution()
+    arr = extend_arr(weak)
+    for i in range(length):
+        for case in permutations(dist, len(dist)):
+            src = 1
+            position = arr[i] + case[src - 1]
+            for j in range(i, i + length):
+                if position < arr[j]:
+                    src += 1
+                    if src > len(dist):
+                        break
+                    position = arr[j] + case[src - 1]
+
+            answer = min(answer, src)
+
+    return answer if answer <= len(dist) else -1
+
