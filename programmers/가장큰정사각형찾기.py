0c58b4ffdb1908d61e58921a8177606175bd06fc
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-05-20 18:51:58 +0900

    Update 가장큰정사각형찾기.py

--- programmers/가장큰정사각형찾기.py
+++ programmers/가장큰정사각형찾기.py
@@ -1,43 +1,32 @@
 import math
 
 
-def my_solution(board):
+def solution(board):
     """
-    problem link:
-        https://school.programmers.co.kr/learn/courses/30/lessons/12905
-
-    solution:
-        1)
-
-    implementations:
-        1) 1행, 1열 넘기기
-        2) 현재 순회하는 행번호의 제곱 만큼이 최대 넓이
-          - 왼쪽 대각선, 왼쪽, 위: 이렇게 3방향이 모두 이전 행번호 제곱과 같으면 넓이는 현재 행번호 제곱
-    => 푸는 컨셉은 맞는데 틀린 문제
-    =>
+    implementation:
+        1) 세방향 중에서 제일 작은 값 찾기
+        2) 모두 제일 작은 값 이상이라면, 한 사이즈 업
     """
-    N, M = len(board), len(board[0])
     answer = 1
-    for y in range(1, N):
-        prev_max = y ** 2
-        for x in range(1, M):
-            if not board[y][x]:
-                continue
+    R, C = len(board), len(board[0])
 
-            UP, LEFT_UP, LEFT = board[y - 1][x], board[y - 1][x - 1], board[y][x - 1]
-            if UP and LEFT_UP and LEFT:
-                if UP >= prev_max and LEFT_UP >= prev_max and LEFT >= prev_max:
-                    board[y][x] = int((y + 1) ** 2)
-                    answer = max(answer, board[y][x])
-                else:
-                    scalar = math.sqrt(min(UP, LEFT_UP, LEFT)) + 1
-                    board[y][x] = int(scalar ** 2)
+    def is_valid(curr_y, curr_x):
+        return board[curr_y][curr_x] and board[curr_y - 1][curr_x] and board[curr_y][curr_x - 1] and board[curr_y - 1][
+            curr_x - 1]
+
+    for y in range(1, R):
+        for x in range(1, C):
+            if is_valid(y, x):
+                size = min(board[y - 1][x], board[y][x - 1], board[y - 1][x - 1])
+                area = (math.sqrt(size) + 1) ** 2
+                answer = max(answer, area)
+                board[y][x] = area
+
+    if answer == 1:
+        for i in range(R):
+            for j in range(C):
+                if board[i][j]:
+                    return answer
+        answer = 0
     return answer
 
-
-def solution(board):
-    pass
-
-
-if __name__ == '__main__':
-    my_solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]])
