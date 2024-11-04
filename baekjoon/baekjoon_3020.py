aa19828e50189ea0584d4b16026cfcf462b34255
JungHwan Lee <qcqced123@gmail.com>
2024-11-04 22:48:41 +0900

    Update baekjoon_3020.py

--- baekjoon/baekjoon_3020.py
+++ baekjoon/baekjoon_3020.py
@@ -6,14 +6,28 @@ def solution():
     석순: 바닥 to 천장
     종유석: 천장 to 바닥
 
+    [0,1]
+    [5,10]
+    [0,3]
+    [7,10]
+    [0,5]
+    [9,10]
+
+    [0,1]
+    [0,3]
+    [0,5]
+    [5,10]
+    [7,10]
+    [9,10]
+
     idea: two pointer, binary search
         - 석순이랑 종유석 단위 통일
-        - 탐색 대상/범위: 구간 배열, 0 to max*2
-        - 탐색 기준
+        - sorting
     """
     input = sys.stdin.readline
     N, H = map(int, input().split())  # 장애물 숫자, 길이
     height = [int(input()) for _ in range(N)]
+
 
 
 
