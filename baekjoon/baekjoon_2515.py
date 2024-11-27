6262df2105c33280780e9d6fe001a436c36522fe
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-11-27 11:30:57 +0900

    add

--- baekjoon/baekjoon_2515.py
+++ baekjoon/baekjoon_2515.py
@@ -1,33 +1,41 @@
 import sys
-from bisect import bisect_left
+from bisect import bisect_right
 
 
 def solution():
     """
     idea: dynamic programming with binary search
-        - 길이 오름 차순 정렬
-        - height 배열 초기화 with bisect
-        - height 배열 이용, dp cache 업데이트
+        - pictures 배열 오름 차순 정렬
+            - 1순위: 높이, 2순위: 비용
+
+        - 개별 그림마다 높이 제약(S) 만족 하는 최대 인덱스 구하기 (bisect_right)
+            - 정렬해서, 높이는 같지만 비용은 더 큰 친구들이 오른쪽에 놓이기 때문에 right를 사용해야 정확하다
+            - right로 얻어낸 인덱스 값에 해당되는 위치의 그림의 높이가 제약 조건과 다르면 1을 빼준 값을 기록
+
+        - 점화식 이용해서 dp 캐시 업데이트
+            - 직전 그림까지의 최대값 vs 현재 그림을 팔 수 있다고 가정하고, 해당 그림과 제약조건을 만족하는 그림까지의 최대 비용을 캐싱
     """
+
     # init the data structure
     input = sys.stdin.readline
     N, S = map(int, input().split())
     pictures = [(0,0)] + [tuple(map(int, input().split())) for _ in range(N)]
-    pictures.sort()
+    pictures.sort()  # 높이랑 비용도 같이 오름차순으로 정렬 되어야 함!!
 
     # init the height array
     cnt = [pictures[i][0] for i in range(N+1)]
     heights = [0]*(N+1)
     for i, (ph, pc) in enumerate(pictures):
-        idx = bisect_left(cnt, ph-S)
-        if ph-S == cnt[idx]: idx += 1
+        idx = bisect_right(cnt, ph-S)
+        if ph-S != cnt[idx]: idx -= 1
         heights[i] = idx
 
     # do update the dp cache with bisect
     cache = [0]*(N+1)
     for i in range(1, N+1):
         j = heights[i]
-        cache[i] = max(cache[i], cache[j]+pictures[i][1])
+        cache[i] = max(cache[i-1], cache[j]+pictures[i][1])
+
     print(max(cache))
 
 
--- test.py
+++ test.py
@@ -20,45 +20,29 @@
 # test = [(2,14,1), (3,8,1), (5,12,1)]
 # test.sort(key=lamb)
 
-from bisect import bisect_left, bisect_right
+N, S = map(int, input().split())
 
-x = 9
-test = [1, 7, 7, 7, 10, 10, 11, 12, 13, 14]
-
-print(bisect_right(test, x))
-print(bisect_left(test, x))
-
-# init the data structure
-INF = sys.maxsize
-input = sys.stdin.readline
-N = int(input())
-visited = [[-1] * N for _ in range(N)]
-dy, dx = (-1, 1, 0, 0), (0, 0, -1, 1)
-grid = [list(map(str, input().rstrip())) for _ in range(N)]
+answer = 0  # 최대합
+array = [[0, 0]]
+dp = [0] * (N + 1)  # i ~ N 번째 중 최대합 금액 저장
+height = [0] * (N + 1)  # i번째 그림을 전시한다고 할 때, 앞에 전시할 수 있는 가장 높은 그림
 
 for i in range(N):
-    for j in range(N):
-        grid[i][j] = int(grid[i][j])
+    H, C = map(int, input().split())
+    array.append([H, C])
+array.sort()  # height 순으로 정렬한다.
 
-# do bfs
-answer = INF
-q = deque([(0, 0)])
-visited[0][0] = 0
-while q:
-    vy, vx = q.popleft()
-    vb = visited[vy][vx]
-    for i in range(4):
-        ny, nx = vy + dy[i], vx + dx[i]
-        if ny == N - 1 and nx == N - 1:
-            answer = min(answer, visited[vy][vx])
-            continue
+for i in range(1, N + 1):
+    height[i] = height[i - 1] + 1
+    while height[i] < i:  # i번 앞에 있는 것들 확인
+        if array[i][0] - array[height[i]][0] < S:  # 조건을 만족하지 못하는 경우
+            break
+        height[i] += 1  # S 간격을 만족하면 index 를 하나 더 늘린다.
+    height[i] -= 1  # i 번째가 조건을 만족하지 못했으므로 -1 한다.
 
-        if -1 < ny < N and -1 < nx < N and vb < answer and (visited[ny][nx] == -1 or vb < visited[ny][nx]):
-            nb = vb
-            if not grid[ny][nx]:
-                nb += 1
+print(height)
 
-            visited[ny][nx] = nb
-            q.append((ny, nx))
+for i in range(1, N + 1):
+    dp[i] = max(dp[i - 1], dp[height[i]] + array[i][1])
 
-print(answer)
+print(dp[N])
