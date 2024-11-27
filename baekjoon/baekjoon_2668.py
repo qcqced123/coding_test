ffc9be7764b7cec0fbf8b64a68a936036c785d68
JungHwan Lee <qcqced123@gmail.com>
2024-11-27 22:10:56 +0900

    Update baekjoon_2668.py

--- baekjoon/baekjoon_2668.py
+++ baekjoon/baekjoon_2668.py
@@ -1,50 +1,50 @@
 import sys
+from collections import defaultdict
 
 
 def solution():
     """
-    idea: backtrack
-        - 탐색 후보군 선정: set(line2)
-        - line1, line2에 대한 추가 배열 생성
-        - 스택 종료 조건:
-        - 스택 호출 조건:
-        - 인자 정의: 현재 탐색 대상 번호
-        - 탐색 대상: line1 and line2 동시에
+    idea: dfs with adj graph, finding cycle
+        - dfs by each node
+        - if there are cycle, then append the current path to result
     """
     # backtrack func
     sys.setrecursionlimit(10**6)
-    def backtrack(cnt: int) -> None:
-        for i in range(cnt, N):
-            cnt_line1 = line1[i]
-            if cnt_line1 not in candidates:
-                continue
-            # 서로 같을 때
-            cnt_line2 = line2[i]
-            if cnt_line1 == cnt_line2:
-                result.append(cnt_line1)
-                backtrack(cnt+1)
-            # 서로 다를 떄
-            else:
-                if not del_line1 or cnt_line2 not in del_line1:
-                    del_line1.add(cnt_line2)
+    def dfs(x, cnt):
+        for nx in graph[x]:
+            if nx == i:
+                result.extend(cnt)
 
+            if nx not in visited:
+                cnt.append(nx)
+                visited.add(nx)
 
-        return
+                dfs(nx, cnt)
+
+                cnt.pop()
+                visited.remove(nx)
 
     # init the data structure
     input = sys.stdin.readline
     N = int(input())
-    line1 = list(range(1, N+1))
-    line2 = [int(input()) for _ in range(N)]
+    arr = [int(input()) for _ in range(N)]
+    graph = defaultdict(list)
 
-    # get candidate
-    candidates = set(line2)
+    for i in range(N):
+        graph[i+1].append(arr[i])
 
-    # do backtrack
+    # find the cycle
     result = []
-    del_line1, del_line2 = set(), set()
-    backtrack(0)
+    visited = set()
+    for i in range(1, N+1):
+        if not i in visited:
+            visited.add(i)
+            dfs(i, [i])
 
+    result.sort()
+    print(len(result))
+    for i in result:
+        print(i)
 
 
 if __name__ == "__main__":
