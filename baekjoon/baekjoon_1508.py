15d8250ecd267ef3adf2d55c183914772af77ea2
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-12-27 14:53:32 +0900

    add

--- baekjoon/baekjoon_1508.py
+++ baekjoon/baekjoon_1508.py
@@ -3,9 +3,53 @@ import sys
 
 def solution():
     """
-    idea:
+    idea: parametric search
+        - 최적화 대상/범위: 심판 사이의 거리, 1, to 10**7
+        - 최적화 기준: mid를 최소값으로 유지 하는데 필요한 심판 숫자
+
+    question:
+        - 정답이 여러개 되는 경우, 그 중에서 한 개는 찾겠는데, 사전순으로 정렬해서 가장 뒤에꺼 출력하려면, 모든 경우의 다 계산 하라는 거자나,,,
+        - K 값이 엄청 작은거 보니까, 백트래킹이나 조합 사용해서 만들어야 하는 것 같은데, 어떻게 코드를 작성할지 감이 안온다
+        - 그 숫자 구슬 문제랑 똑같음
+        => 이게 되네ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+        => 근데 답지 보자, 솔직히 거의 찍어서 맞은거랑 다를바가 없음
+
     """
+    # get input data
     input = sys.stdin.readline
+    N, M, K = map(int, input().split())
+    positions = list(map(int, input().split()))
+
+    # do parametric search
+    answer = 0
+    ans_seq = ""
+    l, r = 1, 10**7
+    while l <= r:
+        mid = (l+r) // 2
+        prev, referee, cache = positions[0], 1, ["1"]
+        for pos in positions[1:]:
+            cnt = pos - prev
+            if cnt < mid:
+                cache.append("0")
+
+            else:
+                prev = pos
+                referee += 1
+                cache.append("1")
+
+            if referee >= M:
+                break
+
+        if referee >= M:
+            l = mid + 1
+            answer = mid
+            ans_seq = cache
+
+        else:
+            r = mid - 1
+
+    result = "".join(ans_seq)
+    print(result + ("0"*(K-len(result))) if len(result) != K else result)
 
 
 if __name__ == "__main__":
--- baekjoon/baekjoon_17472.py
+++ baekjoon/baekjoon_17472.py
@@ -1,12 +1,132 @@
 import sys
+from collections import deque
+from itertools import combinations
 
 
 def solution():
     """
-    idea:
+    idea: adj array search
+        - coloring with bfs
+        - backtracking for optimizing the minimum length of total bridges
+            - calculate the minimum cost of path of node to node
+            - get case by using combinations, selecting the edges such as mst
+    feedback:
+        - 전체 다리 경로에 섬이 존재 하는가 찾아 줘야 함
+    reference:
+        - https://www.acmicpc.net/board/view/119531
+        - https://www.acmicpc.net/board/view/89137
     """
-    input = sys.stdin.readline
+    # bfs func for coloring each area
+    def bfs(y: int, x: int, color: int) -> None:
+        q = deque([(y,x)])
+        while q:
+            vy, vx = q.popleft()
+            for i in range(4):
+                ny, nx = vy + dy[i], vx + dx[i]
+                if -1 < ny < N and -1 < nx < M and grid[ny][nx] == 1:
+                    q.append((ny, nx))
+                    grid[ny][nx] = color
 
+    # backtrack func for optimizing the total length of bridges
+    def dijkstra(start: int, goal: int):
+        src_point = area_pos[start]
+        for y,x in src_point:
+            for s in range(1, max(N, M)):  # bridge size
+                for d in range(4):
+                    ny, nx = y + dy[d], x + dx[d]
+                    if -1 < ny < N and -1 < nx < M and not grid[ny][nx]:
+                        nsy, nsx = ny + s*dy[d], nx + s*dx[d]
+                        nnsy, nnsx = nsy+dy[d], nsx+dx[d]
+                        if -1 < nsy < N and -1 < nsx < M and not grid[nsy][nsx]:
+                            if -1 < nnsy < N and -1 < nnsx < M and grid[nnsy][nnsx] == goal:
+                                for ns in range(1, s+1):
+                                    vny, vnx = ny + ns*dy[d], nx + ns*dx[d]
+                                    if grid[vny][vnx]:
+                                        break
+
+                                else:
+                                    cost[start][goal] = min(cost[start][goal], s+1)
+                                    return
+        return
+
+    # union-find for find disjoint-set
+    def find(x: int):
+        if disjoint[x] != x:
+            disjoint[x] = find(disjoint[x])
+        return disjoint[x]
+
+    def union(y: int, x: int) -> None:
+        y = find(y)
+        x = find(x)
+        if y < x: disjoint[x] = y
+        else: disjoint[y] = x
+
+    # get input data
+    INF = sys.maxsize
+    input = sys.stdin.readline
+    N, M = map(int, input().split())
+    dy, dx = (-1, 1, 0, 0), (0, 0, -1, 1)
+    grid = [list(map(int, input().split())) for _ in range(N)]
+
+    # do coloring
+    num = 2
+    for i in range(N):
+        for j in range(M):
+            if grid[i][j] == 1:
+                grid[i][j] = num
+                bfs(i, j, num)
+                num += 1
+
+    # make the shortest path of each node to node, similar to floyd-warshall
+    cost = [[INF]*num for _ in range(num)]
+    area_pos = {i: [] for i in range(2, num)}
+    for i in range(N):
+        for j in range(M):
+            if grid[i][j]:
+                area_pos[grid[i][j]].append((i,j))
+
+    for src in range(2, num):
+        for end in range(src+1, num):
+            dijkstra(src, end)
+
+    # get combinations of selecting the edge
+    answer = INF
+    for comb in combinations(combinations(range(2, num),2), num-3):
+        flag = 0
+        cache = 0
+        disjoint = [i for i in range(num)]
+        for i,j in comb:
+            if find(i) != find(j):
+                union(i,j)
+            else:
+                flag += 1
+                break
+        if flag:
+            continue
+
+        root = 0
+        for i in range(2, num):
+            if i == disjoint[i]:
+                root += 1
+            if root > 1:
+                break
+        if root > 1:
+            continue
+
+        for i,j in comb:
+            cache += min(cost[i][j], cost[j][i])
+        answer = min(answer, cache)
+
+    print(answer) if answer != INF else print(-1)
+
+    # # check current state of grid after coloring
+    # for i in range(N):
+    #     print(grid[i], end="\n")
+    # print()
+    #
+    # # check current state of cost grid
+    # for i in range(num):
+    #     print(cost[i], end='\n')
 
 if __name__ == "__main__":
     solution()
